8267a942b4a11f71df850b271b75721d671774d2
= <ccreyesmolina@gmail.com>
2024-06-07 10:04:44 -0500

    test

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,5 @@ import react from '@vitejs/plugin-react-swc';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: 'https://sherna0303.github.io/test/'
-  
+  base: 'https://creies.github.io/Pinguinera-Auth-Front/'
 });
